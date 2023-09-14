9e5f6c56c907f04a289f1f162c64a0f368eccc31
unknown <gauravsoman19805@gmail.com>
2023-09-14 10:26:10 +0530

    basic login page design

--- application/lib/main.dart
+++ application/lib/main.dart
@@ -1,18 +1,55 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart';
+
 
 void main(){
-  runApp(const MyApp()); 
-} 
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key}); 
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  LoginPage(),
-    ); 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+      backgroundColor: Colors.blue.shade700,
+        appBar: AppBar(
+          title: const Text("OptiPort"),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                  child: Center(
+                    child: Card(
+                      clipBehavior: Clip.hardEdge,
+                      child: InkWell(
+                        splashColor: Colors.blue.withAlpha(30),
+                        onTap: (){
+                          debugPrint('Card Tapped');
+                        },
+                        child: Center(
+                          widthFactor: 0.8,
+                          child: SizedBox(
+                            width: 600,
+                            height: 80,
+                            child: Center(
+
+                              child: Text("LOGIN")
+                          ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+
+              ),
+            ],
+          ),
+        ),
+      ),
+
+    debugShowCheckedModeBanner: false,
+    );
   }
 }
